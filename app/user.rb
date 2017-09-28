d8a1d24b368579ed2cbcd9335b51e22fe30a8a2b
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-09-28 22:33:15 +0300

    user entity

--- app/user.rb
+++ app/user.rb
@@ -1,3 +1,33 @@
-class User
+require_relative 'actions'
 
+class User
+  include Actions
+  
+  attr_reader :name, :score, :cash, :hands
+  attr_writer :cards
+  
+  def initialize(name)
+    @name = name
+    @score = 0
+    @cash = 100
+    @hands = []
+    @rate = 0
+  end
+
+  def add_card(card)
+    @hands << card
+  end
+  
+  def score
+    rate @hands
+  end
+  
+  def parlay
+    @cash = @cash - 10
+  end
 end
+
+user = User.new '123'
+user.add_card '2'
+user.add_card 'Q'
+puts user.score
